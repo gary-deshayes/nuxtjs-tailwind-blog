51f929052febd545127659e5c27592e516e3fd7c
gary-ubiq <124579847+gary-ubiq@users.noreply.github.com>
2024-08-14 23:26:21 +0200

    feat: change sitemap and remove projects

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -52,7 +52,7 @@ export default defineNuxtConfig({
     },
   },
   sitemap: {
-    hostname: 'portfolio.gary-deshayes.com',
+    hostname: 'https://portfolio.gary-deshayes.com',
     sources: [
       '/api/towns',
     ]
